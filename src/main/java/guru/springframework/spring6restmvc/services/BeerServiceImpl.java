0a783e3b4f1f6e5ec7bc53c8ce9ce8c154517e87
Phuc Tran <ngocphuc1612@gmail.com>
2023-03-28 12:08:02 +0700

    for deploy render

--- src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -22,45 +22,45 @@ public class BeerServiceImpl implements BeerService {
     public BeerServiceImpl() {
         this.beerMap = new HashMap<>();
 
-        Beer beer1 = Beer.builder()
-                .id(UUID.randomUUID())
-                .version(1)
-                .beerName("Galaxy Cat")
-                .beerStyle(BeerStyle.PALE_ALE)
-                .upc("12356")
-                .price(new BigDecimal("12.99"))
-                .quantityOnHand(122)
-                .createdDate(LocalDateTime.now())
-                .updateDate(LocalDateTime.now())
-                .build();
+        // Beer beer1 = Beer.builder()
+        //         .id(UUID.randomUUID())
+        //         .version(1)
+        //         .beerName("Galaxy Cat")
+        //         .beerStyle(BeerStyle.PALE_ALE)
+        //         .upc("12356")
+        //         .price(new BigDecimal("12.99"))
+        //         .quantityOnHand(122)
+        //         .createdDate(LocalDateTime.now())
+        //         .updateDate(LocalDateTime.now())
+        //         .build();
 
-        Beer beer2 = Beer.builder()
-                .id(UUID.randomUUID())
-                .version(1)
-                .beerName("Crank")
-                .beerStyle(BeerStyle.PALE_ALE)
-                .upc("12356222")
-                .price(new BigDecimal("11.99"))
-                .quantityOnHand(392)
-                .createdDate(LocalDateTime.now())
-                .updateDate(LocalDateTime.now())
-                .build();
+        // Beer beer2 = Beer.builder()
+        //         .id(UUID.randomUUID())
+        //         .version(1)
+        //         .beerName("Crank")
+        //         .beerStyle(BeerStyle.PALE_ALE)
+        //         .upc("12356222")
+        //         .price(new BigDecimal("11.99"))
+        //         .quantityOnHand(392)
+        //         .createdDate(LocalDateTime.now())
+        //         .updateDate(LocalDateTime.now())
+        //         .build();
 
-        Beer beer3 = Beer.builder()
-                .id(UUID.randomUUID())
-                .version(1)
-                .beerName("Sunshine City")
-                .beerStyle(BeerStyle.IPA)
-                .upc("12356")
-                .price(new BigDecimal("13.99"))
-                .quantityOnHand(144)
-                .createdDate(LocalDateTime.now())
-                .updateDate(LocalDateTime.now())
-                .build();
+        // Beer beer3 = Beer.builder()
+        //         .id(UUID.randomUUID())
+        //         .version(1)
+        //         .beerName("Sunshine City")
+        //         .beerStyle(BeerStyle.IPA)
+        //         .upc("12356")
+        //         .price(new BigDecimal("13.99"))
+        //         .quantityOnHand(144)
+        //         .createdDate(LocalDateTime.now())
+        //         .updateDate(LocalDateTime.now())
+        //         .build();
 
-        beerMap.put(beer1.getId(), beer1);
-        beerMap.put(beer2.getId(), beer2);
-        beerMap.put(beer3.getId(), beer3);
+        // beerMap.put(beer1.getId(), beer1);
+        // beerMap.put(beer2.getId(), beer2);
+        // beerMap.put(beer3.getId(), beer3);
     }
 
     @Override
